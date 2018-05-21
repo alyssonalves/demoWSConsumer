e6eb79006c3feed34a3fc8e5a0c421c7086feddd
Alves de Lima <a.alves.de.lima@accenture.com>
2018-05-21 16:31:07 -0300

    primeiro commit

---
+++ src/main/java/com/example/demo/Teste.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.List;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+@RestController
+@RequestMapping("api")
+@Produces(MediaType.APPLICATION_JSON)
+public class Teste {
+	
+	@Autowired
+	private ViaCEPClient cliente;
+	
+	
+	@GetMapping("/cossulta/{cep}")
+    public Endereco list(@PathVariable("cep") String cep) {
+		
+		String json = cliente.buscaEnderecoPor(cep);
+
+		Gson gson = new Gson();
+		
+		Endereco e = gson.fromJson(json, Endereco.class);
+		
+	    return e;
+		
+    }
+	
+	
+}
---
+++ src/main/java/com/example/demo/ViaCEPClient.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(url="https://viacep.com.br/ws/", name = "viacep")
+public interface ViaCEPClient {
+	
+	@GetMapping("{cep}/json")
+    String buscaEnderecoPor(@PathVariable("cep") String cep);
+
+}
